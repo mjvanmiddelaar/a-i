d11e945caa525ff8171edecc2bc306fecefa2a4f
Maarten van Middelaar <maarten@duara.nl>
2023-05-20 11:42:07 +0300

    add doc

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
+"""Module determins number of coins for total"""
+
+
 def makeChange(coins, total):
-    """make change"""
+    """Make change function
+
+    Args:
+        coins: first integer
+        total: second integer
+
+    Returns:
+        Total count
+    """
+    if total <= 0:
+        return 0
+
     count = 0
     coins = sorted(coins)
-    while True:
+    while len(coins) > 0:
         coin = coins.pop()
+        if coin > total:
+            continue
         count += int(total / coin)
         total = total % coin
         if total == 0:
             return count
-        if len(coins) == 1:
-            break
     return -1
