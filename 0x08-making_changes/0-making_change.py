d64ff039bde2b137bdbbe30c38635c0ffb10fe48
Maarten van Middelaar <maarten@duara.nl>
2023-05-20 11:28:28 +0300

    add a-i

---
+++ 0x08-making_changes/0-making_change.py
@@ -0,0 +1,10 @@
+def makeChange(coins, total):
+    count = 0
+    coins = sorted(coins)
+    while len(coins) > 1:
+        coin = coins.pop()
+        count += int(total / coin)
+        total = total % coin
+        if total == 0:
+            return count
+    return -1
